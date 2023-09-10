eedb8e7b421da0c8339fcdb97c7f020124ebcaac
Cale <calesecond@gmail.com>
2023-09-10 17:17:53 -0400

    Optimized for testing

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-import { EarthCanvas, StarsCanvas } from './canvas';
+import {StarsCanvas } from './canvas';
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
@@ -6,7 +6,6 @@ import Experience from "./Experience";
 import Works from "./Works";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
-import CanvasLoader from "./Loader";
 import Footer from './Footer';
 
 
@@ -19,7 +18,5 @@ export {
   Feedbacks,
   Contact,
   Footer,
-  CanvasLoader,
-  EarthCanvas, 
   StarsCanvas
 };
